48e706cc9b92b8d604eadfb5cdd9aae95e973279
tcrepy <tanguycrepy@gmail.com>
2017-12-19 10:51:52 +0100

    Comparaison de l'etat du port GPIO et de la BDD

--- blink.js
+++ blink.js
@@ -1,4 +1,4 @@
-// var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
+let Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
 // var LED = new Gpio(4, 'out'); //use GPIO pin 12, and specify that it is output
 // var blinkInterval = setInterval(blinkLED, 250); //run the blinkLED function every 250ms
 
@@ -9,7 +9,7 @@ let initialData;
 let params = {};
 
 request({
-    url: "http://localhost:8080/select",
+    url: "http://149.202.41.94:8080/select",
     method: "POST",
     json: true,
     body: params
@@ -25,15 +25,16 @@ request({
         }, function (error, response, body) {
 
             //on verifie qu'il n'y a pas eu de suppression ou d'ajout de lampes
-            if (body.length !== initialData.length){
+            if (body.length !== initialData.length) {
                 initialData = body;
             }
             else {
-                for (let i = 0; i < body.length; i++){
+                for (let i = 0; i < body.length; i++) {
                     //on verifie l'etat de la lampe, si il a changé, on allume ou on eteint
                     //verification de LED.read() si il est dans le meme etat que la bdd
-                    if (body[i]['SW_BOOL'] !== initialData[i]['SW_BOOL']){
+                    if (body[i]['SW_BOOL'] !== initialData[i]['SW_BOOL']) {
                         //TODO::Faire le LED.write()
+                        blinkLED(body[i]);
                         console.log(body[i]['SW_GPIO_ID']);
                         initialData = body;
                     }
@@ -45,13 +46,12 @@ request({
 });
 
 
-// function blinkLED() { //function to start blinking
-//   if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
-//     LED.writeSync(1); //set pin state to 1 (turn LED on)
-//   } else {
-//     LED.writeSync(0); //set pin state to 0 (turn LED off)
-//   }
-// }
+function blinkLED(body) { //function to start blinking
+    let LED = new Gpio(body['SW_GPIO_ID'], 'OUT');
+    if (LED.readSync() !== body['SW_BOOL']) { //check the pin state, if the state is 0 (or off)
+        LED.writeSync(body['SW_BOOL']); //set pin state to 1 (turn LED on)
+    }
+}
 //
 // function endBlink() { //function to stop blinking
 //   clearInterval(blinkInterval); // Stop blink intervals
