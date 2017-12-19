335cb0a92ae3f543875c82b8a0aa9dde32fc2279
tcrepy <tanguycrepy@gmail.com>
2017-12-19 11:38:37 +0100

    Essai de creation de plusieurs Gpio

--- blink.js
+++ blink.js
@@ -1,5 +1,5 @@
 let Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
-let LED = new Gpio(4, 'out'); //use GPIO pin 12, and specify that it is output
+ //use GPIO pin 12, and specify that it is output
 // var blinkInterval = setInterval(blinkLED, 250); //run the blinkLED function every 250ms
 
 let request = require('request');
@@ -8,12 +8,18 @@ let initialData;
 
 let params = {};
 
+let LED = [];
+
 request({
     url: "http://149.202.41.94:8080/select",
     method: "POST",
     json: true,
     body: params
 }, function (error, response, body) {
+    for (let t = 0; t < body.length; t++){
+        LED[body[i]['SW_ID']] = new Gpio(body[i]['SW_GPIO_ID'], 'out');
+    }
+    console.log(LED);
     initialData = body;
     console.log(initialData);
     setInterval(function () {
@@ -27,6 +33,8 @@ request({
             //on verifie qu'il n'y a pas eu de suppression ou d'ajout de lampes
             if (body.length !== initialData.length) {
                 initialData = body;
+                //TODO::new gpio() avec les valeurs de la nouvelle row
+                //TODO::delete gpio correspondant si manque une row
             }
             else {
                 for (let i = 0; i < body.length; i++) {
@@ -47,16 +55,7 @@ request({
 
 
 function blinkLED(body) { //function to start blinking
-    if (LED.readSync() != body['SW_BOOL']) { //check the pin state, if the state is 0 (or off)
-        LED.writeSync(body['SW_BOOL']); //set pin state to 1 (turn LED on)
+    if (LED[body['SW_ID']].readSync() != body['SW_BOOL']) { //check the pin state, if the state is 0 (or off)
+        LED[body['SW_ID']].writeSync(body['SW_BOOL']); //set pin state to 1 (turn LED on)
     }
 }
-//
-// function endBlink() { //function to stop blinking
-//   clearInterval(blinkInterval); // Stop blink intervals
-//   LED.writeSync(0); // Turn LED off
-//   LED.unexport(); // Unexport GPIO to free resources
-//   console.log('endBlink');
-// }
-//
-// setTimeout(endBlink, 5000); //stop blinking after 5 seconds
